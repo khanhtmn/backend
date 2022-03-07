a341c9b906de85d8bde2d85cee73f72719a57dac
Khanh Nguyen <khanh@minerva.kgi.edu>
2022-03-07 15:54:09 -0800

    update postgres URL in config

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 """Flask configuration."""
 
-SQLALCHEMY_DATABASE_URI = "postgres://myrajytibhmqgb:1ac2c8ac415153568ac08ed0ab7a901d799f0fd7946e58373620c6c025f61e37@ec2-44-194-167-63.compute-1.amazonaws.com:5432/debcq0srbm4h05"
+SQLALCHEMY_DATABASE_URI = "postgresql://myrajytibhmqgb:1ac2c8ac415153568ac08ed0ab7a901d799f0fd7946e58373620c6c025f61e37@ec2-44-194-167-63.compute-1.amazonaws.com:5432/debcq0srbm4h05"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = "Th1s1ss3cr3t"
