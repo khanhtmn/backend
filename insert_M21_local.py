9cd17dbcb3b4cce4016cf64291a7e069e05116ea
Khanh Nguyen <khanh@minerva.kgi.edu>
2022-03-30 14:38:30 -0700

    working scripts from M19 to M22

--- insert_M21_local.py
+++ insert_M21_local.py
@@ -8,13 +8,13 @@ from utils import create_list_of_data
 
 #### MAIN ####
 # Import csv file for students' data
-df = pd.read_csv(r'/home/khanh/Documents/capstone-hub-backend/scripts/M21_Capstone _information.csv')
+df = pd.read_csv(r'/home/khanh/Documents/capstone-hub-backend/csv_files/M21_Capstone _information.csv')
 
 # Logins model
 logins_cols = ["id", "public_id", "email"]
 logins_data_cols = ["id", "id","email"]
 logins_default_cols = ["password"]
-logins_default_vals = ["Minerva22"]
+logins_default_vals = ["Minerva21"]
 logins_to_add = create_list_of_data(\
     data=df,\
     data_cols=logins_data_cols,\
@@ -23,39 +23,29 @@ logins_to_add = create_list_of_data(\
     default_vals=logins_default_vals\
     )
 
-# # UserProject model
-# users_cols = ["id", "login_id", "name",\
-#             "primary_major", "secondary_major",\
-#             "primary_concentration", "secondary_concentration",\
-#             "special_concentration", "minor", "minor_concentration"]
-# users_data_cols = ["id", "id", "name", "project_link",\
-#                 "primary_major", "second_major",\
-#                 "primary_concentration", "second_concentration",\
-#                 "special_concentration", "minor", "minor_concentration", "additional_information"]
-
 # Columns in projects model
 user_projects_cols = ["id", "user_id", "name",\
             "primary_major", "secondary_major",\
             "primary_concentration", "secondary_concentration",\
             "special_concentration", "minor", "minor_concentration",\
-            "title", "abstract",\
+            "project_link", "title", "abstract",\
             "keywords", "feature", "hsr_review", "skills",\
             "los", "custom_los", "advisor", "skills_offering",\
-            "skills_requesting", "location", "last_updated"]
+            "skills_requesting", "additional_information", "last_updated"]
 
 # Columns from CSV file
 user_projects_data_cols = ["id", "id", "name",\
             "primary_major", "second_major",\
             "primary_concentration", "second_concentration",\
             "special_concentration", "minor", "minor_concentration",\
-            "title", "abstract",\
+            "project_link", "title", "abstract",\
             "keywords", "features", "hsr_status", "skills",\
             "los", "custom_los", "advisor", "skills_offering",\
-            "skills_requesting", "location", "timestamp"]
+            "skills_requesting", "additional_information", "timestamp"]
 
 # Default columns and their default values
 user_projects_default_cols = ["class_year"]
-user_projects_default_vals = [2022]
+user_projects_default_vals = [2021]
 
 # Create list of data from all columns and values above
 user_projects_to_add = create_list_of_data(\
